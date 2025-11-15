d3e41242d9296b7bd45922b07688d4cf7fea3927
Murlidhar Varma <murlidhar.varma@gmail.com>
2025-11-15 21:26:58 +0530

    added minio, mlflow containers

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# shut all running container
 docker compose down
+
+# complete clean up all
 rm -rf ./runtime/airflow/data
 rm -rf ./runtime/postgres/data
+rm -rf ./runtime/minio/data
+rm -rf ./runtime/mlflow/data
+
+# start the containers
 docker compose up --build -d
